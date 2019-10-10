1c7b630870528777450f53f4367e6f81334a7897
rajivsah <rajiv.shah01234@gmail.com>
2019-10-10 13:20:28 +0545

    add blogpost.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,6 +2,7 @@ class BlogController < ActionController::Base
   protect_from_forgery with: :exception
 
   def homepage
-    render json: {msg: 'Hello World'}, status: 200
+    posts = BlogPost.all
+    render json: {posts: posts.to_json, msg: 'success' }, status: 200
   end
 end
